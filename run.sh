8fafdfda2e949632d6eab61fe352026ca71c60bb
Helvi Lainekallio <helvi.lainekallio@gmail.com>
2021-03-12 13:27:15 +0200

    subject added

--- run.sh
+++
@@ -1,8 +0,0 @@
-norm_output=$(~/.norminette/norminette.rb)
-printf "%s" "$norm_output"
-grep "Error" <<< "$norm_output" > /dev/null
-if [ $? == 0 ]; then
-	printf "%b" "\n\nFix norm and try again\n";
-elif make ; then
-	valgrind --leak-check=yes --track-origins=yes ./minishell
-fi
--- run2.sh
+++
@@ -1,3 +0,0 @@
-if make debug ; then
-	valgrind --leak-check=yes --track-origins=yes ./ft_select hi hello
-fi
